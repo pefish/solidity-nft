ccc204a1903c7b1814fc54ff557992368fa810b5
pefish <pefish@qq.com>
2022-01-20 09:20:31 +0800

    add

--- contracts/Nft.sol
+++ contracts/Nft.sol
@@ -15,40 +15,42 @@ import "@pefish/solidity-lib/contracts/contract/Ownable.sol";
  */
 contract Nft is Erc721Enumerable, Ownable {
     string public baseURI;
-    uint256 public cost = 0.06 ether;
+    uint256 public cost = 1000 ether;
     uint256 public whitelistCost = 0 ether;
     uint256 public maxSupply;
     uint256 public maxMintNum = 20;
-    bool public paused = true;
+    bool public paused = false;
     address[] public whitelistedAddresses;
 
     constructor(
         string memory _name,
         string memory _symbol,
-        uint256 _maxSupply
+        uint256 _maxSupply,
+        string memory _baseURI
     ) Erc721(_name, _symbol) {
         Ownable.__Ownable_init();
 
         maxSupply = _maxSupply;
+        baseURI = _baseURI;
     }
 
     // public
-    function mint(uint256 _mintAmount) public payable {
+    function mint(uint256 _mintNum) public payable {
         require(!paused, "the contract is paused");
         uint256 supply = totalSupply();
-        require(_mintAmount > 0, "need to mint at least 1 NFT");
-        require(_mintAmount <= maxMintNum, "max mint amount per session exceeded");
-        require(supply + _mintAmount <= maxSupply, "max NFT limit exceeded");
+        require(_mintNum > 0, "need to mint at least 1 NFT");
+        require(_mintNum <= maxMintNum, "max mint amount per session exceeded");
+        require(supply + _mintNum <= maxSupply, "max NFT limit exceeded");
 
         if (msg.sender != owner()) {
             if(isWhitelisted(msg.sender)) {
-                require(msg.value >= whitelistCost * _mintAmount, "insufficient funds");
+                require(msg.value >= whitelistCost * _mintNum, "insufficient funds");
             } else {
-                require(msg.value >= cost * _mintAmount, "insufficient funds");
+                require(msg.value >= cost * _mintNum, "insufficient funds");
             }
         }
 
-        for (uint256 i = 1; i <= _mintAmount; i++) {
+        for (uint256 i = 1; i <= _mintNum; i++) {
             _safeMint(msg.sender, supply + i);
         }
     }
--- migrations/1_deploy_nft_contract.ts
+++ migrations/1_deploy_nft_contract.ts
@@ -5,7 +5,7 @@ import Web3 from "web3"
 declare var web3: Web3;
 
 module.exports = async function (deployer,network, accounts) {
-  await deployer.deploy(Nft, "TEST", "test", 100);
+  await deployer.deploy(Nft, "FBEE", "Fil Bee", 20, "ipfs://QmSMoHBEdqhbgZbnHHPDk4f5mLmTRjrSAz7HubbbfaL63e/");
 } as Truffle.Migration;
 
 export {};
